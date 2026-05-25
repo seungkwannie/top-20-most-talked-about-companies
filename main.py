354ea72dcc9240e6f40bfc47938e2f88ef8d4223
myreguiang <aveguiang123@gmail.com>
2026-05-25 21:11:35 +0800

    "new files"

--- main.py
+++ main.py
@@ -3,20 +3,6 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 
-# st.title("TOP 20 MOST TALKED ABOUT COMPANIES")
-#
-# def top_20_companies():
-#     # st.header("Fetching news articles...")
-#     news = data_ingestor.fetch_trending_news()
-#     # st.header("Processing news articles...")
-#     processed_news = nlp_processor.process_text_frequencies(news)
-#     # st.header("Displaying results...")
-#     # st.header("TOP 20 MOST TALKED ABOUT COMPANIES ===")
-#     for rank, (company, mentions) in enumerate(processed_news, start=1):
-#         st.header(f"{rank}. {company} — Mentioned {mentions} times")
-#
-# st.button("Show Top 20 Companies", on_click=top_20_companies)
-
 st.set_page_config(page_title="Company Trends", page_icon="📈", layout="centered")
 
 st.markdown(
